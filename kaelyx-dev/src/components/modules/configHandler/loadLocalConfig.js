d561dd62d8479b2fee3ddb330c4d9b0a3f55b12b
Kaelyx <scott-donaldson@outlook.com>
2025-04-25 16:27:13 +0100

    Started commenting out the config refactor

---
+++ kaelyx-dev/src/components/modules/configHandler/loadLocalConfig.js
@@ -0,0 +1,5 @@
+// convert all to conf files from json files
+// get local config folder
+// get all files in local config folder
+// add them to a kv map.
+// swap out where theyre used with the getConfig method.
