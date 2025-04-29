c26440dc31f92f093ac13a77e2c8d8a1cbf4550f
Kaelyx <scott-donaldson@outlook.com>
2025-04-29 10:44:35 +0100

    Added meta parsing for article type and meta passthrough for set values

--- kaelyx-dev/src/components/modules/Content/utilities/meta.js
+++ kaelyx-dev/src/components/modules/Content/utilities/meta.js
@@ -1,24 +1,30 @@
 const metaObj = {
-    Title    : undefined,
-    Type     : undefined,
-    Date     : undefined,
-    Keywords : undefined
+    Title        : undefined,
+    Type         : undefined,
+    Date         : undefined,
+    Keywords     : undefined,
+    showPermalink: undefined,
+    showReadTime : undefined
 }
 
 const generateDefaultMetaObj = () => {
     let metaObj = structuredClone(metaObj)
-    let defaultValues = {Type: "GENERAL"}
+    let defaultValues = {Type: "GENERAL", showPermalink: false, showReadTime: false}
 
     Object.keys(defaultValues).forEach(e => metaObj[e] = defaultValues[e])
+
     return metaObj
 }
 
 export const parseMeta = meta => {
-    let meta = generateDefaultMetaObj()
+    let newMeta = generateDefaultMetaObj()
 
+    Object.keys(meta).forEach(e => newMeta[e] = metaObj[e])
 
-    // do the meta parsing stuff here
-
-
+    if(meta.type.toUpperCase() == "ARTICLE") {
+        newMeta.showPermalink = true
+        newMeta.showReadTime  = true
+    }
+    
     return meta
 }
