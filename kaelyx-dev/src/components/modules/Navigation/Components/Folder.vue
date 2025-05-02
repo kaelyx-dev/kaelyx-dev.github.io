11db09ea46a502cb1115685b2b74c6de14055fd8
Scrub <scott-donaldson@outlook.com>
2025-05-02 23:56:32 +0100

    Nested nav folders show if active page is in their subtree if closed

--- kaelyx-dev/src/components/modules/Navigation/Components/Folder.vue
+++ kaelyx-dev/src/components/modules/Navigation/Components/Folder.vue
@@ -1,22 +1,21 @@
 <script setup>
 import { defineProps, ref } from 'vue';
 import File from './File.vue';
+import { useDirectoryStore } from '@/stores/DirectoryStore';
+
+let directory = useDirectoryStore()
 
 const { folder,root } = defineProps({
     folder: Object,
     root  : Boolean
 })
 
-
 const isClosed = ref(!root)
-
-console.log(isClosed.value)
-
 const toggle = () => isClosed.value = !isClosed.value
 
 </script>
 <template>
-    <p @click="toggle" class="folder-name"><input type="checkbox" class="folder-checkbox" v-if="!root" v-model="isClosed"/>{{ folder.metaName }}</p>
+    <p @click="toggle" class="folder-name"><input type="checkbox" class="folder-checkbox" v-if="!root" v-model="isClosed" /><span :class="{selected : directory.activeContentUrl.startsWith(folder.link) && isClosed}">{{ folder.metaName }}</span></p>
     <ul class="nav-tree" :class="{closed : isClosed}">
         <li v-if="Object.keys(folder.folders).length > 0" v-for="(subfolder, index) in folder.folders" :key="index">
             <Folder :folder="subfolder"/>
