aef7042db35aa9551cca892fdc7ee00a446932f4
Kaelyx <scott-donaldson@outlook.com>
2025-05-15 15:59:35 +0100

    404 spa redirects (attempted)

--- kaelyx-dev/src/components/modules/Content/utilities/console.js
+++ kaelyx-dev/src/components/modules/Content/utilities/console.js
@@ -1,19 +1,11 @@
-export const command = command => {
-    return "Hello World"
-}
-
 export const parsePath = (cwd, input) => {
-    // If input is absolute, treat it as-is
     let combined;
-    if (input.startsWith("/")) {
-        combined = input;
-    } else {
-        // Ensure cwd ends with "/"
+    if (input.startsWith("/")) combined = input;
+    else {
         if (!cwd.endsWith("/")) cwd += "/";
         combined = cwd + input;
     }
 
-    // Normalize the path manually (resolving "." and "..")
     const parts = combined.split("/");
     const stack = [];
 
